5ae2d9caf164b2879f97e269529cfd6d75fe8e81
rmsp7223123 <129022648+rmsp7223123@users.noreply.github.com>
2023-09-23 20:23:06 +0900

    data/user 추가

---
+++ app/src/main/java/com/example/kotlin_todo2/data/User.kt
@@ -0,0 +1,18 @@
+package com.example.kotlin_todo2.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+
+// @Entity annotation을 붙여주었기때문에, Room database가 이 dataclass가 Entity라는 걸 인식
+@Entity(tableName = "user_table") // 테이블 이름 설정
+data class User (
+    @PrimaryKey(autoGenerate = true) // pk인 id는 자동으로 만들게
+    val id : Int, // 컬럼1
+    val firstName:String, // 컬럼2
+    val lastName:String, // 컬럼3
+    val age:Int // 컬럼4
+) {
+
+}
